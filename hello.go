a68ad7d33a0f6c3e7a0c10ff4c2fa3108ab49633
Alex Macniven <macniven.ap@gmail.com>
2022-03-05 13:15:33 +0000

    Add Error Handling And Log Package

--- hello.go
+++ hello.go
@@ -2,9 +2,11 @@
 package main
 
 // Here we have imports.
-// The 'fmt' package gives us formatting functions like Print.
 import (
+	// The 'fmt' package supports formatting functions like Print.
 	"fmt"
+	// The 'log' package supports log messages and configs.
+	"log"
 	// greetings is currently stored on our local disk.
 	// For development purposes we can create an alias that will
 	// point "example.com/greetings" at the local package instead
@@ -19,7 +21,16 @@ import (
 
 // When you run a package, it's main function will be called.
 func main() {
+	// Set a logging prefix.
+	log.SetPrefix("greetings: ")
+	// Set a flag to disable the time being printed.
+	log.SetFlags(0)
 	// We're calling Hello() from the greetings package.
-	message := greetings.Hello("Alex")
+	// We'll also capture any errors returned.
+	message, err := greetings.Hello("Alex")
+	// If an error is returned we log it with Fatal so the program exits.
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
